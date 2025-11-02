5d5fa5d1e348818c837f70a0cfb2df8f29ceac4d
Aaro Paltemaa <apaltemaa@gmail.com>
2025-11-02 11:33:41 +0200

    Update greeting message in the root route and adjust Docker Compose ports for beermapping service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World V2!</h1>");
+  res.send("<h1>Hello updated world!</h1>");
 });
 
 const PORT = 8080;
